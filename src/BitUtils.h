8972b4250ff763b991bea950048e4466080ee39d
TesseracT33 <christoffer.ekgrim98@hotmail.com>
2021-12-16 13:38:11 +0100

    Add start-up files

---
+++ src/BitUtils.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <concepts>
+
+namespace BitUtils
+{
+	template<std::integral T> inline constexpr bool CheckBit(T num, unsigned pos)
+	{
+		return num & (1ull << pos);
+	}
+
+	template<std::integral T> inline constexpr bool CheckBit(T* num, unsigned pos)
+	{
+		return *num & (1ull << pos);
+	}
+
+	template<std::integral T> inline constexpr void SetBit(T& num, unsigned pos)
+	{
+		num |= (1ull << pos);
+	}
+
+	template<std::integral T> inline constexpr void SetBit(T* num, unsigned pos)
+	{
+		*num |= (1ull << pos);
+	}
+
+	template<std::integral T> inline constexpr void ClearBit(T& num, unsigned pos)
+	{
+		num &= ~(1ull << pos);
+	}
+
+	template<std::integral T> inline constexpr void ClearBit(T* num, unsigned pos)
+	{
+		*num &= ~(1ull << pos);
+	}
+
+	template<std::integral T> inline constexpr void ToggleBit(T& num, unsigned pos)
+	{
+		num ^= ~(1ull << pos);
+	}
+
+	template<std::integral T> inline constexpr void ToggleBit(T* num, unsigned pos)
+	{
+		*num ^= ~(1ull << pos);
+	}
+}
---
+++ src/MIPS4300i.h
@@ -0,0 +1,4 @@
+#pragma once
+class MIPS4300i
+{
+};
