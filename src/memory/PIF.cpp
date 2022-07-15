0e74206398733f104dd23b95ac5ba9e9b419fcd4
Christoffer Ekgrim <christoffer.ekgrim98@hotmail.com>
2022-07-15 15:39:06 +0200

    Part of joybus

--- src/memory/PIF.cpp
+++ src/memory/PIF.cpp
@@ -9,6 +9,24 @@ import Util.Files;
 
 namespace PIF
 {
+	void ChallengeProtection()
+	{
+
+	}
+
+
+	void ChecksumVerification()
+	{
+		memory[command_byte_index] |= 0x80;
+	}
+
+
+	void ClearRam()
+	{
+		std::fill(memory.begin() + ram_start, memory.end(), 0);
+	}
+
+
 	size_t GetNumberOfBytesUntilRAMEnd(const u32 offset)
 	{
 		return ram_size - (offset & (ram_size - 1)); /* Size is 0x40 bytes */
@@ -29,13 +47,14 @@ namespace PIF
 
 	bool LoadIPL12(const std::string& path)
 	{
-		std::optional<std::array<u8, rom_size>> optional_rom = FileUtils::LoadBinaryFileArray<rom_size>(path);
+		std::optional<std::array<u8, ram_size + rom_size>> optional_rom =
+			FileUtils::LoadBinaryFileArray<ram_size + rom_size>(path);
 		if (!optional_rom.has_value()) {
 			UserMessage::Show("Failed to open boot rom (IPL) file.", UserMessage::Type::Warning);
 			return false;
 		}
 		const auto& rom = optional_rom.value();
-		std::memcpy(memory.data(), rom.data(), rom_size);
+		std::memcpy(memory.data(), rom.data(), ram_size + rom_size);
 		return true;
 	}
 
@@ -49,13 +68,58 @@ namespace PIF
 	}
 
 
+	void RunJoybusProtocol()
+	{
+		switch (memory[ram_start]) {
+		case 0x00:/* Info */
+		case 0xFF:
+			/* Device: controller */
+			memory[ram_start] = 0x05;
+			memory[ram_start + 1] = 0x00;
+			/* Pak installed */
+			memory[ram_start + 2] = 0x01;
+			break;
+
+		case 0x01: /* Controller State */
+			std::memcpy(memory.data() + ram_start, &joypad_status, 4);
+			break;
+
+		case 0x02:
+			break;
+
+		case 0x03:
+			break;
+
+		case 0x04:
+			break;
+
+		case 0x05:
+			break;
+		}
+	}
+
+
 	template<size_t number_of_bytes>
 	void WriteMemory(u32 addr, auto data)
 	{ /* CPU precondition: write does not go to the next boundary */
 		addr &= 0x7FF;
-		if (addr >= rom_size) { /* $0-$7BF: rom; $7C0-$7FF: ram */
+		if (addr >= ram_start) { /* $0-$7BF: rom; $7C0-$7FF: ram */
 			data = std::byteswap(data);
 			std::memcpy(memory.data() + addr, &data, number_of_bytes);
+			if (addr + number_of_bytes > 0x7FF) {
+				if (memory[command_byte_index] & 0x01) {
+					RunJoybusProtocol();
+				}
+				if (memory[command_byte_index] & 0x02) {
+					ChallengeProtection();
+				}
+				if (memory[command_byte_index] & 0x20) {
+					ChecksumVerification();
+				}
+				if (memory[command_byte_index] & 0x40) {
+					ClearRam();
+				}
+			}
 		}
 	}
 
--- src/memory/PIF.ixx
+++ src/memory/PIF.ixx
@@ -26,8 +26,17 @@ namespace PIF
 		void WriteMemory(u32 addr, auto data);
 	}
 
+	void ChallengeProtection();
+	void ChecksumVerification();
+	void ClearRam();
+	void RunJoybusProtocol();
+
+	constexpr size_t command_byte_index = 0x7FF;
 	constexpr size_t ram_size = 0x40;
 	constexpr size_t rom_size = 0x7C0;
+	constexpr size_t ram_start = rom_size;
+
+	s32 joypad_status;
 
 	std::array<u8, rom_size + ram_size> memory{}; /* $0-$7BF: rom; $7C0-$7FF: ram */
 }
